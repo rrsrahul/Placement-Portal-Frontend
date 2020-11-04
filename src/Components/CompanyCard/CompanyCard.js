beadb8147deabcbc9c17dabb828ba82aa7bfb447
Rahul R S <rahulrsgoku@gmail.com>
2020-11-05 02:43:07 +0530

    Cleanup

---
+++ src/Components/CompanyCard/CompanyCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import classes from './CompanyCard.module.css'
+
+const companyCard = (props)=>
+{
+    return (
+        <div className={classes.Card}>
+                CompanyCard
+        </div>
+    )
+}
+
+export default companyCard;
---
+++ src/Containers/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import CompanyCard from '../Components/CompanyCard/CompanyCard';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Hello</h1>
+      <CompanyCard>
+
+      </CompanyCard>
+    </div>
+  );
+}
+
+export default App;
