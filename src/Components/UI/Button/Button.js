e69ab2480338af86fe3e9424cdbd76f65280f8ab
Vikram K <vikram.k@gametheory.in>
2021-01-07 15:05:42 +0530

    UI update

--- src/Components/UI/Button/Button.js
+++ src/Components/UI/Button/Button.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import classes from './Button.module.css';
+import React from "react";
+import classes from "./Button.module.css";
 
-const button = (props)=>
-{
-    return (
-<button className={[classes.Button,classes[props.btnType]].join(' ')}
-disabled={props.disabled}
-onClick={props.clicked}>
-{props.children}
-</button>
-    );
-}
+const button = (props) => {
+  return (
+    <div style={{padding: 10}}>
+      <button
+        className={props.className}
+        disabled={props.disabled}
+        onClick={props.clicked}
+      >
+        {props.children}
+      </button>
+    </div>
+  );
+};
 
 export default button;
