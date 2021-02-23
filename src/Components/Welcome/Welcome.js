898f5ba55fc847fb09626ecb4aadbeff8ca6c4ac
Vikram K <vikram.k@gametheory.in>
2021-02-24 00:27:11 +0530

    welcome pg added db link to usn and dept

--- src/Components/Welcome/Welcome.js
+++ src/Components/Welcome/Welcome.js
@@ -11,14 +11,28 @@ const Welcome = (props) => {
     return state.auth.userData.name;
   });
 
+  let usn = useSelector((state) => {
+    if (!state.auth.userData) {
+      return "user";
+    };
+    return state.auth.userData.usn;
+  });
+
+  let dept = useSelector((state) => {
+    if (!state.auth.userData) {
+      return "user";
+    };
+    return state.auth.userData.branch;
+  });
+
   return (
     <div className={classes.center}>
       <p className={classes.Welcome}>WELCOME TO BMSCE CAMPUS PORTAL</p>
       <hr></hr>
       <p style={{padding:'20px'}}></p>
       <p className={classes.info}>Name : {name}</p>
-      <p className={classes.info}>USN : 1bm17is089</p>
-      <p className={classes.info}>Department : ISE</p>
+      <p className={classes.info}>USN : {usn}</p>
+      <p className={classes.info}>Department : {dept}</p>
     </div>
   );
 };
