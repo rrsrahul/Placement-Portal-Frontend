56aa16b9635f909c443962d931a06d8bf29d5210
Rahul R S <rahulrsgoku@gmail.com>
2021-02-24 02:37:18 +0530

    Reactive SideDrawer UI

--- src/Components/Navigation/ToolBar/ToolBar.js
+++ src/Components/Navigation/ToolBar/ToolBar.js
@@ -2,11 +2,13 @@ import React from 'react';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './ToolBar.module.css'
 import Logo from '../../Logo/Logo';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle'
 
 const toolbar= (props)=>
 {
     return (
         <header className={classes.ToolBar}>
+          <DrawerToggle clicked={props.clicked}/>
           <Logo height="80%"/>
           <nav className={classes.DesktopOnly} >
             <NavigationItems isAuth={props.isAuth} isAdmin={props.isAdmin}/>
